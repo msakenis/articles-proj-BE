c0bd1d3f793696f6d456be76bd15a13833ef277e
msakenis <mindaugas@pitchground.com>
2021-07-05 09:30:21 +0300

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const userActionsRoute = require('./routes/userActions');
+
+const port = process.env.SERVER_PORT || 4000;
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cors());
+
+app.use('/', userActionsRoute);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
